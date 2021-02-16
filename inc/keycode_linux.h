7cb2449332accdcc415449b1fbbb9adc2e9d7ed8
tinaserra <vserra@student.42.fr>
2021-02-16 07:45:16 +0100

    [add] keycode linux

---
+++ inc/keycode_linux.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keycode_linux.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/16 07:37:47 by vserra            #+#    #+#             */
+/*   Updated: 2021/02/16 07:43:57 by vserra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYCODE_LINUX_H
+# define KEYCODE_LINUX_H
+
+/* Linux qwerty */
+
+// # define KEY_ESCAPE 
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+# define KEY_W 119
+# define KEY_S 115
+# define KEY_D 100
+# define KEY_A 97
+
+/* Linux azerty */
+
+// # define KEY_W 122
+// # define KEY_A 113
+
+#endif
