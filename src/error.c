5b09355e4dcd77f7b322b91f33b8d9c7505bd104
tinaserra <vserra@student.42.fr>
2021-03-21 17:35:23 +0100

    [fix] double free

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 08:46:03 by vserra            #+#    #+#             */
-/*   Updated: 2021/03/15 14:09:34 by vserra           ###   ########.fr       */
+/*   Updated: 2021/03/21 15:51:16 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,21 @@ void	free_map(t_env *env)
 {
 	int		i;
 
-	i = 0;
 	printf("JE FREE LA MAP LA CON DE TA MERE\n");
-	while (i < env->map.nb_lines)
+	i = 0;
+	while (i < env->map.nb_lines - 1)
 	{
 		if (env->map.map[i] != NULL)
+		{
+			env->map.map[i] = NULL;
 			free(env->map.map[i]);
+		}
 		i++;
 	}
 	free(env->map.map);
 }
 
-int print_error(t_env *env, int error)
+int		print_error(t_env *env, int error)
 {
 	free_textures(env);
 	if (env->map.map != NULL)
@@ -51,6 +54,5 @@ int print_error(t_env *env, int error)
 	if (env->spr)
 		free(env->spr);
 	ft_putstr_fd(g_str_error[error], 2);
-	// system("leaks Cub3D");
 	exit(EXIT_FAILURE);
 }
